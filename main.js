a9c1fd085f6ee99f44e058cef18730a4e8c3251e
Mrbusy <nickhughes13@googlemail.com>
2022-11-03 15:28:08 +0000

    codewars tests started

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 /* 
 👉 Write your kata here!
+You've just bought a new house and the bathroom needs renovating urgently. You need to tile one wall completely (floor to ceiling and side to side). You are on a budget and can only afford to buy exactly the right amount of tiles. How many do you need?
+x = length of wall
+y = height of wall
+a = length of tile
+b = height of tile
+For anyone that has done some tiling, don't worry about grout joints in your calculation!
+Remember walls and tiles will all be greater than zero in any given dimension. If any dimension is zero or negative return there has been a measurement error, so you should return null.
 */
-
 //👉 Write the function your CodeWarriors will start with below here:
+export function howManyTiles ([x,y,a,b]) {
+    let wall = x*y
+    let tile = a*b
+    let total = wall/tile
+    numberOfTiles = Math.ceil(total)
+    return numberOfTiles
+    }
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,9 @@
 //👉 Write your tests below here:
+import { expect, test } from "@jest/globals"
+import {howManyTiles} from './main.js'
+
+test(`When given a dimension including zero, the return is null.`, () => {
+    const actual = (howManyTiles([3,0,0.6,0.3]));
+    const expected = "null";
+  
+    expect(actual).toBe(expected);})
