0cd2bd2ebb6f9506bbcc1ee6c291f231905096ac
rhonamackay <rhonamackay@outlook.com>
2022-11-03 16:14:08 +0000

    Finished kata ready for attempts

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 /* 
-👉 Write your kata here!
 You've just bought a new house and the bathroom needs renovating urgently. You need to tile one wall completely (floor to ceiling and side to side). You are on a budget and can only afford to buy exactly the right amount of tiles. How many do you need?
 x = length of wall
 y = height of wall
@@ -9,18 +8,6 @@ Walls and tiles should always be greater than zero in any given dimension - if a
 You should return a whole number of tiles with no decimal point.
 For anyone that has done some real life tiling, don't worry about grout joints in your calculation!
 */
-//👉 Write the function your CodeWarriors will start with below here:
 export function howManyTiles ([x,y,a,b]) {
-    let input = [x,y,a,b]
-    let filter = input.filter(x => x <= 0)
-    if (filter.length !== 0) {
-        return null
-    }
-    let wall = x*y
-    let tile = a*b
-    let total = wall/tile
-    let numberOfTiles = Math.ceil(total)
     return numberOfTiles
     }
-
-console.log(`Test: ${howManyTiles([3,1.5,3,0.2])}`)
